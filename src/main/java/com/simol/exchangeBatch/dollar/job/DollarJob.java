fbefa46e35dcea2d0854e1c5ad8912940651c6e0
juno <juno.c@kakaovx.com>
2024-08-05 10:13:18 +0900

    feat: job 틀 만들기

---
+++ src/main/java/com/simol/exchangeBatch/dollar/job/DollarJob.java
@@ -0,0 +1,41 @@
+package com.simol.exchangeBatch.dollar.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class DollarJob {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public Job dollarJobs() {
+        return new JobBuilder("DollarJobStart", jobRepository)
+                .start(dollarStep())
+                .build();
+    }
+
+    @Bean
+    public Step dollarStep() {
+        return new StepBuilder("dollarStep", jobRepository)
+                .tasklet(tasklet(), platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Tasklet tasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("dollar job start");
+            return null;
+        };
+    }
+}
